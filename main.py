a0ac4f039240a2fbc4c1b40f7fe8074c286e703c
Akeem <asbirkett@yahoo.ca>
2025-02-04 20:42:33 -0500

    - renamed word_count function to get_word_count
    - renamed distinct_character_count to get_dictinct_character_count
    
    -added print_book_report function

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ def read_book(path_to_file):
         file_contents = f.read()
         return file_contents
 
-def word_count(string):
-    content_array = string.split()
-    print(len(content_array))
+def get_word_count(book_content):
+    content_array = book_content.split()
+    return len(content_array)
 
-def get_distinct_character_count(string):
-    character_count = len(string)
+def get_distinct_character_count(book_content):
+    character_count = len(book_content)
     character_count_dictionary = {}
     for i in range(0, character_count):
-        lower_case_char = string[i].lower()
+        lower_case_char = book_content[i].lower()
         if lower_case_char not in character_count_dictionary:
             character_count_dictionary[lower_case_char] = 1
         else:
@@ -19,12 +19,20 @@ def get_distinct_character_count(string):
 
     return character_count_dictionary
 
+def print_book_report(word_count, character_count_dictionary):
+    print(f'{word_count} words found in the document')
+    print()
+    for dic in character_count_dictionary:
+        if(dic.isalpha()):
+            print(f'The \'{dic}\' character was found {character_count_dictionary[dic]} times')
+
+
 def main():
     book_content = read_book('./books/frankenstein.txt')
-    # word_count(book_content)
+    book_word_count = get_word_count(book_content)
     distinct_character_count_result = get_distinct_character_count(book_content)
-
-    print(distinct_character_count_result)
+    print_book_report(book_word_count, distinct_character_count_result)
+    
 
 
 main()
